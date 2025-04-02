9ca24ed25808ec810f48391146e5cfbbbfa01e55
OhYoonjin <95potter95@gmail.com>
2025-04-02 23:02:22 +0900

    Add files via upload
    2021년 한동대학교 SW Festival 오목 대회 우승작 upload

---
+++ alpha_6-main/src/alpha_6/cor.java
@@ -0,0 +1,28 @@
+package alpha_6;
+
+public class cor {
+	int x, y;
+	cor(int x, int y){
+		this.x=x;
+		this.y=y;
+	}
+	cor(){
+		this.x=0;
+		this.y=0;
+	}
+	public int getX() {
+		return this.x;
+	}
+	public int getY() {
+		return this.y;
+	}
+	public void setX(int x) {
+		this.x=x;
+		return;
+	}
+	public void setY(int y) {
+		this.y=y;
+		return;
+	}
+	
+}
---
+++ alpha_6-main/src/alpha_6/mainTester.java
@@ -0,0 +1,9 @@
+package alpha_6;
+
+public class mainTester {
+	public static void main(String[] args) {
+		
+		BoardGUI board = new BoardGUI();
+		board.show();
+	}
+}
